972f0ea131645ef57547c964aad2f9bab72a959d
liyueqing <liyueqing@bingosoft.net>
2019-03-01 17:14:30 +0800

    fix: 注释入口文件

--- src/entry/app.js
+++ src/entry/app.js
@@ -1,4 +1,6 @@
+// 入口*.vue文件，对象命名为“App”，在编译文件"webpack.common.conf.js"里引用
 import App from 'src/views/app.vue'
+// 引入bui-weex
 import buiweex from 'src/js/buiweex';
 Vue.use(buiweex);
 
--- src/entry/flow-demo.js
+++ src/entry/flow-demo.js
@@ -1,3 +1,4 @@
+// 入口*.vue文件，对象命名为"App"，在编译文件"webpack.common.conf.js"里引用
 import App from 'src/views/example/flow-demo.vue'
 import buiweex from 'src/js/buiweex.js';
 Vue.use(buiweex);
